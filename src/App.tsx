78af85eca5660d6e63c96bf9ad58191dceee77b9
Arthur Danielson <arthurdanielson7@gmail.com>
2025-05-20 11:14:04 -0400

    added Header and Footer

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,22 @@
 import React from "react";
 
+// components
+import { Header } from "./components/Header";
+import { Footer } from "./components/Footer";
+
+// CSS
+import styles from "./App.module.css";
+
 function App() {
-  return <div>TODO REACT</div>;
+  return (
+    <div>
+      <Header />
+      <main className={styles.main}>
+        <h1>Conteúdo...</h1>
+      </main>
+      <Footer />
+    </div>
+  );
 }
 
 export default App;
